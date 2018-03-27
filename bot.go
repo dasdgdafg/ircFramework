80d219230e7f9c3db1ae7d1ecc009469b1a550a0
dsdf <dasdgdafg@cock.li>
2018-03-27 19:17:33 -0400

    reconnect after losing connection

--- bot.go
+++ bot.go
@@ -28,7 +28,12 @@ type IRCBot struct {
 	MessageHandler   func(linesToSend chan<- string, nick string, channel string, msg string)
 	ExtraLinesToSend chan string
 
-	state channelState
+	state               channelState
+	userLinesToSend     chan string
+	retryUserLine       string
+	internalLinesToSend chan string
+	dbWrites            chan dbOp
+	logger              *log.Logger
 }
 
 func (b *IRCBot) Run() {
@@ -40,7 +45,7 @@ func (b *IRCBot) Run() {
 	log.Println("starting up")
 
 	t := time.Now()
-	ts := t.Format("Jan 2 2006 15-04-05 ET")
+	ts := t.Format("2006/01/02 15-04-05")
 
 	logFilename := "logs/" + ts + ".txt"
 	f, err := os.Create(logFilename)
@@ -51,43 +56,81 @@ func (b *IRCBot) Run() {
 	// output to both stdout and the log file by default
 	logWriter := io.MultiWriter(os.Stdout, f)
 	log.SetOutput(logWriter)
-	fileOnlyLogger := log.New(f, "", log.Flags())
+	b.logger = log.New(f, "", log.Flags())
 
-	socket, err := tls.Dial("tcp", b.Server+":"+b.Port, &tls.Config{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer socket.Close()
-	log.Println("socket connected")
+	b.userLinesToSend = make(chan string, 5)  // lines the user wants to send
+	b.internalLinesToSend = make(chan string) // lines generated internally by this framework
+	b.dbWrites = make(chan dbOp, 2)
 
-	errors := make(chan bool)
-	linesToSend := make(chan string, 5)
-	dbWrites := make(chan dbOp, 2)
-	go b.readLines(socket, errors, linesToSend, fileOnlyLogger, dbWrites)
-	go writeLines(socket, errors, linesToSend, fileOnlyLogger)
+	go b.reconnect()
+
 	if b.ListenToStdin {
-		go manualInput(linesToSend)
+		go b.manualInput()
 	}
-	go b.writeToDb(dbWrites)
-	linesToSend <- "NICK " + b.Nickname
-	linesToSend <- "USER " + b.Ident + " * 8 :" + b.Realname
+	go b.writeToDb()
 
 	if b.ExtraLinesToSend != nil {
 		go func() {
 			for {
-				// send any values on ExtraLinesToSend over to linesToSend
-				linesToSend <- (<-b.ExtraLinesToSend)
+				// send any values on ExtraLinesToSend over to userLinesToSend
+				b.userLinesToSend <- (<-b.ExtraLinesToSend)
 			}
 		}()
 	}
 
-	<-errors
+	<-make(chan int)
+}
+
+// create a new socket and start read/writing it
+func (b *IRCBot) reconnect() {
+	kys := make(chan struct{}) // used to signal that the connection is dead and goroutines using it should stop
+	socket, err := tls.Dial("tcp", b.Server+":"+b.Port, &tls.Config{})
+	if err != nil {
+		log.Println(err)
+		go b.disconnected(kys, socket)
+		return
+	}
+	log.Println("socket connected")
+	// start reading and writing internal lines, but don't write user lines yet because they won't succeed until after we've joined the irc channels
+	go b.readLines(kys, socket)
+	go b.writeInternalLines(kys, socket)
+	// try to send the nick and ident
+	select {
+	case b.internalLinesToSend <- "NICK " + b.Nickname:
+		select {
+		case b.internalLinesToSend <- "USER " + b.Ident + " * 8 :" + b.Realname:
+		case _, _ = <-kys:
+		}
+	case _, _ = <-kys:
+	}
+}
+
+func (b *IRCBot) connected(kys chan struct{}, socket net.Conn) {
+	// safe to write the user lines now
+	go b.writeUserLines(kys, socket)
+}
+
+func (b *IRCBot) disconnected(kys chan struct{}, socket net.Conn) {
+	select {
+	case _, _ = <-kys:
+		// channel already closed, do nothing
+		return
+	default:
+		// channel is still open, close it and the socket
+		if socket != nil {
+			socket.Close()
+		}
+		close(kys)
+		// retry with exponential backoff
+		// TODO: exp backoff
+		b.reconnect()
+	}
 }
 
 // ensure db writes are done sequentially
-func (b *IRCBot) writeToDb(dbWrites <-chan dbOp) {
+func (b *IRCBot) writeToDb() {
 	for {
-		job := <-dbWrites
+		job := <-b.dbWrites
 		if job.add {
 			b.state.AddChannel(job.channel)
 		} else {
@@ -97,71 +140,108 @@ func (b *IRCBot) writeToDb(dbWrites <-chan dbOp) {
 }
 
 // listen to stdin and send any lines over the socket
-func manualInput(linesToSend chan<- string) {
+func (b *IRCBot) manualInput() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatalln(err)
 		}
-		linesToSend <- text
+		b.userLinesToSend <- text
 	}
 }
 
-func (b *IRCBot) readLines(socket net.Conn, errors chan<- bool, linesToSend chan<- string, logFile *log.Logger, dbWrites chan<- dbOp) {
+func (b *IRCBot) readLines(kys chan struct{}, socket net.Conn) {
 	reader := bufio.NewReader(socket)
 	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-			errors <- true
+		select {
+		case _, _ = <-kys:
 			return
+		default:
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				log.Println(err)
+				go b.disconnected(kys, socket)
+				return
+			}
+			// remove the trailing \r\n
+			line = line[:len(line)-2]
+			b.logger.Println(">>> " + line) // only print these to the log file, not to the default logger
+			b.processLine(kys, socket, line)
 		}
-		// remove the trailing \r\n
-		line = line[:len(line)-2]
-		logFile.Println(">>> " + line) // only print these to the log file, not to the default logger
-		b.processLine(line, linesToSend, dbWrites)
 	}
 }
 
-func writeLines(socket net.Conn, errors chan<- bool, linesToSend <-chan string, logFile *log.Logger) {
-	writer := bufio.NewWriter(socket)
+func (b *IRCBot) writeUserLines(kys chan struct{}, socket net.Conn) {
+	if b.retryUserLine != "" {
+		line := b.retryUserLine
+		b.logger.Println("(retry) <<< " + line) // only print these to the log file, not to the default logger
+		_, err := socket.Write([]byte(line + "\n"))
+		if err != nil {
+			log.Println(err)
+			go b.disconnected(kys, socket)
+			return
+		}
+	}
 	for {
-		line := <-linesToSend
-		logFile.Println("<<< " + line) // only print these to the log file, not to the default logger
-		_, err := writer.WriteString(line + "\n")
+		var line string
+		select {
+		case _, _ = <-kys:
+			return
+		case line = <-b.userLinesToSend:
+		}
+		b.logger.Println("<<< " + line) // only print these to the log file, not to the default logger
+		_, err := socket.Write([]byte(line + "\n"))
 		if err != nil {
 			log.Println(err)
-			errors <- true
+			b.retryUserLine = line
+			go b.disconnected(kys, socket)
 			return
 		}
-		// make sure it actually gets sent
-		writer.Flush()
 	}
 }
 
-func (b *IRCBot) processLine(line string, linesToSend chan<- string, dbWrites chan<- dbOp) {
+func (b *IRCBot) writeInternalLines(kys chan struct{}, socket net.Conn) {
+	for {
+		var line string
+		select {
+		case _, _ = <-kys:
+			return
+		case line = <-b.internalLinesToSend:
+		}
+		b.logger.Println("<<< " + line) // only print these to the log file, not to the default logger
+		_, err := socket.Write([]byte(line + "\n"))
+		if err != nil {
+			log.Println(err)
+			go b.disconnected(kys, socket)
+			return
+		}
+	}
+}
+
+func (b *IRCBot) processLine(kys chan struct{}, socket net.Conn, line string) {
+	var lineToSend string
 	strs := strings.SplitN(line, " ", 4)
 	if len(strs) >= 2 {
 		if strs[0] == "PING" {
 			// PING :message
-			linesToSend <- "PONG :" + afterColon(strs[1])
+			lineToSend = "PONG :" + afterColon(strs[1])
 		} else if strs[1] == "376" || strs[1] == "422" {
 			// :server 376 nick :End of /MOTD command.
-			linesToSend <- "PRIVMSG nickserv :identify " + b.Password
+			lineToSend = "PRIVMSG nickserv :identify " + b.Password
 		} else if strs[1] == "396" && strs[2] == b.Nickname {
 			// :server 396 nick host :is now your visible host
 			log.Println("joining channels")
-			go b.joinChannels(linesToSend)
+			go b.joinChannels(kys, socket)
 		} else if strs[1] == "PRIVMSG" {
 			// :nick!ident@host PRIVMSG #channel :message
-			b.processPrivmsg(linesToSend, extractNick(strs[0]), strs[2], afterColon(strs[3]))
+			b.processPrivmsg(extractNick(strs[0]), strs[2], afterColon(strs[3]))
 		} else if strs[1] == "INVITE" && strs[2] == b.Nickname {
 			// :nick!ident@host INVITE nick :#channel
 			channel := afterColon(strs[3])
 			log.Println("joining " + channel + ", invited by " + strs[0])
-			linesToSend <- "JOIN " + channel
-			dbWrites <- dbOp{true, channel}
+			b.dbWrites <- dbOp{true, channel}
+			lineToSend = "JOIN " + channel
 		} else if strs[1] == "KICK" {
 			// :nick!ident@host KICK #channel nick :message
 			nickAndMsg := strings.SplitN(strs[3], " ", 2)
@@ -171,12 +251,19 @@ func (b *IRCBot) processLine(line string, linesToSend chan<- string, dbWrites ch
 					message = nickAndMsg[1]
 				}
 				log.Println("kicked from " + strs[2] + " by " + strs[0] + " because " + message)
-				dbWrites <- dbOp{false, strs[2]}
+				b.dbWrites <- dbOp{false, strs[2]}
 			}
 		} else if strs[1] == "474" && strs[2] == b.Nickname {
 			// :server 474 nick #channel :Cannot join channel (+b)
 			log.Println("can't join " + strs[3])
-			dbWrites <- dbOp{false, strings.Split(strs[3], " ")[0]}
+			b.dbWrites <- dbOp{false, strings.Split(strs[3], " ")[0]}
+		}
+	}
+	if lineToSend != "" {
+		select {
+		case _, _ = <-kys:
+			return
+		case b.internalLinesToSend <- lineToSend:
 		}
 	}
 }
@@ -200,14 +287,21 @@ func afterColon(str string) string {
 	return ""
 }
 
-func (b *IRCBot) joinChannels(linesToSend chan<- string) {
+func (b *IRCBot) joinChannels(kys chan struct{}, socket net.Conn) {
+	// TODO: check for kys, set connected once done
 	for _, ch := range b.state.GetChannels() {
-		linesToSend <- "JOIN " + ch
+		select {
+		case _, _ = <-kys:
+			return
+		case b.internalLinesToSend <- "JOIN " + ch:
+		}
 	}
+	// now that the irc channels have been joined, we're done setting up the connection
+	go b.connected(kys, socket)
 }
 
-func (b *IRCBot) processPrivmsg(linesToSend chan<- string, nick string, channel string, msg string) {
+func (b *IRCBot) processPrivmsg(nick string, channel string, msg string) {
 	if b.MessageHandler != nil {
-		b.MessageHandler(linesToSend, nick, channel, msg)
+		b.MessageHandler(b.userLinesToSend, nick, channel, msg)
 	}
 }
